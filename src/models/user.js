f7c0f62db517c2610e2d03750b8ac83dd33ac193
João Rosmaninho <jv_ros@hotmail.com>
2020-12-14 12:09:19 -0300

    Models criadas e uso da Uuid
    Dar um npm install pra atualizar a uuid

---
+++ src/models/user.js
@@ -0,0 +1,30 @@
+const { v4:uuidv4 } = require("uuid");
+const connection = require("../database/connection");
+
+module.exports = {
+    async create(){
+        const user_id = uuidv4();
+        user.user_id = user_id;
+        
+        const result = await connection("user").insert(user);
+        return result;
+    },
+    async getById({user_id}){
+        const result = await connection("user")
+        .where({user_id})
+        .select("*");
+        return result;
+    },
+    async updateById(user_id,user){
+        const result = await connection("user")
+        .where({user_id})
+        .update(user);
+        return result;
+    },
+    async deleteById(){
+        const result = await connection("user")
+        .where({user_id})
+        .delete();
+    }
+    
+}
